3a22f390ac2516fac2212a13f81d0bd769cfc466
Shailendra-debug <shailendrakushwaha732@gmail.com>
2026-04-25 10:48:00 +0530

    Update DB render to Relawy 2.0

--- src/main/java/getintouch/com/GetInTouch/GetInTouchApplication.java
+++ src/main/java/getintouch/com/GetInTouch/GetInTouchApplication.java
@@ -5,13 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableMethodSecurity(prePostEnabled = true)
 
 public class GetInTouchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GetInTouchApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+        SpringApplication.run(GetInTouchApplication.class, args);
 	}
 
 
